dc541a6914f93357f91cb898606389c9a4df165b
daniel-munoz13 <daniel.munoz13@upr.edu>
2026-02-16 22:59:52 -0400

    PHASE1 1: 1.1 Done

--- src/Program.cpp
+++ src/Program.cpp
@@ -15,10 +15,11 @@ Program::Program() {
             std::pair<float, float>{600, 150}, 
             new SpEnemy(600, 150)
         });
-
+        
+    //Aligns the enemies in the starting fase
     for (int i = 0; i < 30; i++) {
-        float x = 250 + 50 * i;
-        float y = 200 + 50 * i;
+        float x = 250 + 50 * (i%10);
+        float y = 200 + 50 * (i/10);
 
         Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
             std::pair<float, float>{x, y}, 
