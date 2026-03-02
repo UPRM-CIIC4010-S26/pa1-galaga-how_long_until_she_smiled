d1e7df2e1b6a16ac29e62d701df69c1479dc3597
hector-g-lopez64 <hector.lopez64@upr.edu>
2026-03-02 00:15:05 -0400

    Implement new wave functionality as benchmark for player progress

--- src/Entities/Score/Score.cpp
+++ src/Entities/Score/Score.cpp
@@ -12,12 +12,18 @@ void ScoreManager::draw(raylib::Vector2 position) {
     raylib::Vector2 hiScoreOffset = raylib::Vector2(250, 0);
     FontManager::PixelFontBody.DrawText("Hi Score", position + hiScoreOffset, baseSize, 0.1f, RED);
     FontManager::PixelFontBody.DrawText(TextFormat("%05i", this->highScore), position + hiScoreOffset + raylib::Vector2(0, 20), baseSize, 0.1, WHITE);
+
+    // BONUS: Draw current wave
+    raylib::Vector2 waveOffset = raylib::Vector2(GetScreenWidth() * 3/4, 0);
+    FontManager::PixelFontBody.DrawText(TextFormat("Wave %02i", this->wave), position + waveOffset, baseSize, 0.1f, RED);
 }
 
 // ----- Adding to score -----
 void ScoreManager::addToScore(uint32_t increment) {
     int scoreUpdate = this->score + increment;
     if (scoreUpdate > this->maxScore) scoreUpdate = this->maxScore;
+
+    if (score >= toNextWave) this->nextWave();
     this->score = scoreUpdate;
 }
 uint32_t ScoreManager::addToScore(uint32_t increment, uint32_t amountToNextLife) {
@@ -30,4 +36,8 @@ uint32_t ScoreManager::addToScore(uint32_t increment, uint32_t amountToNextLife)
     int livesObtained = ((thousandRangeUpdate) ? 1 : 0);
 
     return livesObtained;
+}
+void ScoreManager::nextWave(void) {
+    this->wave++;
+    this->toNextWave += this->waveRate;
 }
