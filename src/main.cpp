4f0933c6a162800627c1743035e8c22467bd4ecb
daniel-munoz13 <daniel.munoz13@upr.edu>
2026-03-13 16:13:17 -0400

    ADD: new comments documenting what the matrix stuff does. REMOVE: Debuging variables of the screen sizes

--- src/main.cpp
+++ src/main.cpp
@@ -24,20 +24,30 @@ int main ()
 
 	raylib::RenderTexture2D target(GetRenderWidth(), GetRenderHeight());
 	OverlayTexture crtEffect("shaders/crt.fs", (float)GetRenderWidth(), (float)GetRenderHeight());
-	float scale = (float)GetRenderWidth() / (float)GetScreenWidth();
+	float scale = (float)GetRenderWidth() / (float)GetScreenWidth();//(1)
+
+	/*
+	So because we arent using RayLib normal shader, that means that GetScreenHeight() or GetScreenWidth() will give us the logical correct dimensions but not 
+	the actual render dimensions (if for example a iMac M1 with a 4k screen will not draw it correctly and actually, will draw like 2000x2000 bc of the highdp 
+	setting so it will be in the half) we need to get the render dimensions of what are we drawing and scale it up to the screen that we want (1000x1000).
+	*/
 
 	while (!WindowShouldClose()) {
-		printf("RenderTexture: %d x %d\n", target.texture.width, target.texture.height);
-		printf("Screen: %d x %d\n", GetScreenWidth(), GetScreenHeight());
 		Galaga.Update();
 
 	target.BeginMode();
 		ClearBackground(BLACK);
+
+		/*
+		As we said previously, we need to scale it. We first calculate the scale by (float) GetRender___()/(float)GetScreen___() (___ being height or width (we
+		chosed Width))(1). Then, we take the State of OpenGl (or the Transforming Matrix) and take to memory specificly the Matrix Stack (2). After we scale the matrix (3),
+		we draw with every x and y scaled (4), and finally get rid of the matrix that we took the Matrix Stack(5). (as u can see this whole shaders stuff was painful)
+		*/
 	
-		rlPushMatrix();
-			rlScalef(scale, scale, 1.0f);
-			Galaga.Draw();
-		rlPopMatrix();
+		rlPushMatrix();//(2)
+			rlScalef(scale, scale, 1.0f); //(3)
+			Galaga.Draw();//(4)
+		rlPopMatrix();//(5)
 		
 	target.EndMode();
 
