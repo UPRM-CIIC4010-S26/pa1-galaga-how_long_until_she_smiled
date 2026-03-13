f68fb473170a8f25bd6bdf6039a003746b3689cc
hector-g-lopez64 <hector.lopez64@upr.edu>
2026-03-12 21:40:23 -0400

    FIX: Migrate to alternate fully open-source CRT filter, implement changes which migrate it from Shadertoy architecture to raylib

--- resources/shaders/crt.fs
+++ resources/shaders/crt.fs
@@ -1,109 +1,244 @@
-// Note: Shader borrowed from public shaders on Shadertoy 
-// Created by frutbunn in 2016-01-15
-// https://www.shadertoy.com/view/XdyGzR
+//
+// PUBLIC DOMAIN CRT STYLED SCAN-LINE SHADER
+//
+//	 by Timothy Lottes
+//
+// This is more along the style of a really good CGA arcade monitor.
+// With RGB inputs instead of NTSC.
+// The shadow mask example has the mask rotated 90 degrees for less chromatic aberration.
+//
+// Left it unoptimized to show the theory behind the algorithm.
+//
+// It is an example what I personally would want as a display option for pixel art games.
+// Please take and use, change, or whatever.
+//
+// NOTE (HECTOR): To work with Raylib, I have changed a few of the values around and added, 
+// a few new ones, but the underlying code has stayed the same. (Denoted by 'H:')
+// from: Shadertoy, https://www.shadertoy.com/view/4scSR8
 
-#define CURVATURE 1.
-#define SCANLINES 1.
-#define CURVED_SCANLINES 1.
-#define BLURED 1.
-#define LIGHT 1.
-#define COLOR_CORRECTION 1.
-//#define ASPECT_RATIO 1.
+// H: Version not needed for Shadertoy, but is required for raylib
+#version 330
 
-const float gamma = 1.;
-const float contrast = 1.;
-const float saturation = 1.;
-const float brightness = 1.;
+#define RGBA(r, g, b, a)	vec4(float(r)/255.0, float(g)/255.0, float(b)/255.0, float(a)/255.0)
 
-const float light = 9.;
-const float blur = 1.5;
+const vec3 kBackgroundColor = RGBA(0x00, 0x60, 0xb8, 0xff).rgb; // medium-blue sky
+//const vec3 kBackgroundColor = RGBA(0xff, 0x00, 0xff, 0xff).rgb; // test magenta
 
-vec3 postEffects(in vec3 rgb, in vec2 xy) {
-    rgb = pow(rgb, vec3(gamma));
-    rgb = mix(vec3(.5), mix(vec3(dot(vec3(.2125, .7154, .0721), rgb*brightness)), rgb*brightness, saturation), contrast);
+// H: Implicit/built-in value iChannel0 on Shadertoy needs to be defined here
+// Additionally, iResolution has to be passed through to the shader
+uniform sampler2D texture; 
 
-    return rgb;
+#define iChannel0 texture
+uniform vec2 iResolution;
+
+// Emulated input resolution.
+#if 1
+	// Fix resolution to set amount.
+	// Note: 256x224 is the most common resolution of the SNES, and that of Super Mario World.
+	vec2 res = vec2(
+		256.0 / 1.0,
+		224.0 / 1.0
+	);
+#else
+	// Optimize for resize.
+	vec2 res = iResolution.xy / 6.0;
+#endif
+
+// Hardness of scanline.
+//	-8.0 = soft
+// -16.0 = medium
+float sHardScan = -8.0;
+
+// Hardness of pixels in scanline.
+// -2.0 = soft
+// -4.0 = hard
+const float kHardPix = -3.0;
+
+// Display warp.
+// 0.0 = none
+// 1.0 / 8.0 = extreme
+const vec2 kWarp = vec2(1.0 / 32.0, 1.0 / 24.0);
+//const vec2 kWarp = vec2(0);
+
+// Amount of shadow mask.
+float kMaskDark = 0.5;
+float kMaskLight = 1.5;
+
+//------------------------------------------------------------------------
+
+// sRGB to Linear.
+// Assuing using sRGB typed textures this should not be needed.
+float toLinear1(float c) {
+	return (c <= 0.04045) ?
+		(c / 12.92) :
+		pow((c + 0.055) / 1.055, 2.4);
+}
+vec3 toLinear(vec3 c) {
+	return vec3(toLinear1(c.r), toLinear1(c.g), toLinear1(c.b));
 }
 
-// Sigma 1. Size 3
-vec3 gaussian(in vec2 uv) {
-    float b = blur / (iResolution.x / iResolution.y);
-
-    uv+= .5;
-
-    vec3 col = texture(iChannel0, vec2(uv.x - b/iResolution.x, uv.y - b/iResolution.y) ).rgb * 0.077847;
-    col += texture(iChannel0, vec2(uv.x - b/iResolution.x, uv.y) ).rgb * 0.123317;
-    col += texture(iChannel0, vec2(uv.x - b/iResolution.x, uv.y + b/iResolution.y) ).rgb * 0.077847;
-
-    col += texture(iChannel0, vec2(uv.x, uv.y - b/iResolution.y) ).rgb * 0.123317;
-    col += texture(iChannel0, vec2(uv.x, uv.y) ).rgb * 0.195346;
-    col += texture(iChannel0, vec2(uv.x, uv.y + b/iResolution.y) ).rgb * 0.123317;
-
-    col += texture(iChannel0, vec2(uv.x + b/iResolution.x, uv.y - b/iResolution.y) ).rgb * 0.077847;
-    col += texture(iChannel0, vec2(uv.x + b/iResolution.x, uv.y) ).rgb * 0.123317;
-    col += texture(iChannel0, vec2(uv.x + b/iResolution.x, uv.y + b/iResolution.y) ).rgb * 0.077847;
-
-    return col;
+// Linear to sRGB.
+// Assuing using sRGB typed textures this should not be needed.
+float toSrgb1(float c) {
+	return(c < 0.0031308 ?
+		(c * 12.92) :
+		(1.055 * pow(c, 0.41666) - 0.055));
+}
+vec3 toSrgb(vec3 c) {
+	return vec3(toSrgb1(c.r), toSrgb1(c.g), toSrgb1(c.b));
 }
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
+// Nearest emulated sample given floating point position and texel offset.
+// Also zero's off screen.
+vec4 fetch(vec2 pos, vec2 off)
 {
-    vec2 st = (fragCoord.xy / iResolution.xy) - vec2(.5);
+	pos = floor(pos * res + off) / res;
+	if (max(abs(pos.x - 0.5), abs(pos.y - 0.5)) > 0.5) 
+		return vec4(vec3(0.0), 0.0);
+   	
+    vec4 sampledColor = texture(iChannel0, pos.xy, -16.0);
     
-    // Curvature/light
-    float d = length(st*.5 * st*.5);
-#ifdef CURVATURE
-    vec2 uv = st*d + st*.935;
-#else
-    vec2 uv = st;
-#endif
-
-    // Fudge aspect ratio
-#ifdef ASPECT_RATIO
-    uv.x *= iResolution.x/iResolution.y*.75;
-#endif
+    sampledColor = vec4(
+        (sampledColor.rgb * sampledColor.a) +
+        	(kBackgroundColor * (1.0 - sampledColor.a)),
+        1.0
+    );
     
-    // CRT color blur
-#ifdef BLURED
-    vec3 color = gaussian(uv);
-#else
-    vec3 color = texture(iChannel0, uv+.5).rgb;
-#endif
+	return vec4(
+        toLinear(sampledColor.rgb),
+        sampledColor.a
+    );
+}
 
-    // Light
-#ifdef LIGHT
-    float l = 1. - min(1., d*light);
-    color *= l;
-#endif
+// Distance in emulated pixels to nearest texel.
+vec2 dist(vec2 pos) {
+	pos = pos * res;
+	return -((pos - floor(pos)) - vec2(0.5));
+}
 
-    // Scanlines
-#ifdef CURVED_SCANLINES
-    float y = uv.y;
-#else
-    float y = st.y;
-#endif
+// 1D Gaussian.
+float gaus(float pos, float scale) {
+	return exp2(scale * pos * pos);
+}
 
-    float showScanlines = 1.;
-    if (iResolution.y<360.) showScanlines = 0.;
+// 3-tap Gaussian filter along horz line.
+vec3 horz3(vec2 pos, float off)
+{
+	vec3 b = fetch(pos, vec2(-1.0, off)).rgb;
+	vec3 c = fetch(pos, vec2( 0.0, off)).rgb;
+	vec3 d = fetch(pos, vec2(+1.0, off)).rgb;
+	float dst = dist(pos).x;
+	// Convert distance to weight.
+	float scale = kHardPix;
+	float wb = gaus(dst - 1.0, scale);
+	float wc = gaus(dst + 0.0, scale);
+	float wd = gaus(dst + 1.0, scale);
+	// Return filtered sample.
+	return (b * wb + c * wc + d * wd) / (wb + wc + wd);
+}
+
+// 5-tap Gaussian filter along horz line.
+vec3 horz5(vec2 pos, float off)
+{
+	vec3 a = fetch(pos, vec2(-2.0, off)).rgb;
+	vec3 b = fetch(pos, vec2(-1.0, off)).rgb;
+	vec3 c = fetch(pos, vec2( 0.0, off)).rgb;
+	vec3 d = fetch(pos, vec2(+1.0, off)).rgb;
+	vec3 e = fetch(pos, vec2(+2.0, off)).rgb;
+	float dst = dist(pos).x;
+	// Convert distance to weight.
+	float scale = kHardPix;
+	float wa = gaus(dst - 2.0, scale);
+	float wb = gaus(dst - 1.0, scale);
+	float wc = gaus(dst + 0.0, scale);
+	float wd = gaus(dst + 1.0, scale);
+	float we = gaus(dst + 2.0, scale);
+	// Return filtered sample.
+	return (a * wa + b * wb + c * wc + d * wd + e * we) / (wa + wb + wc + wd + we);
+}
+
+// Return scanline weight.
+float scan(vec2 pos, float off) {
+	float dst = dist(pos).y;
+	return gaus(dst + off, sHardScan);
+}
+
+// Allow nearest three lines to effect pixel.
+vec3 tri(vec2 pos)
+{
+	vec3 a = horz3(pos, -1.0);
+	vec3 b = horz5(pos,  0.0);
+	vec3 c = horz3(pos, +1.0);
+	float wa = scan(pos, -1.0);
+	float wb = scan(pos,  0.0);
+	float wc = scan(pos, +1.0);
+	return a * wa + b * wb + c * wc;}
+
+// Distortion of scanlines, and end of screen alpha.
+vec2 warp(vec2 pos)
+{
+	pos = pos * 2.0 - 1.0;
+	pos *= vec2(
+		1.0 + (pos.y * pos.y) * kWarp.x,
+		1.0 + (pos.x * pos.x) * kWarp.y
+	);
+	return pos * 0.5 + 0.5;
+}
+
+// Shadow mask.
+vec3 mask(vec2 pos)
+{
+	pos.x += pos.y * 3.0;
+	vec3 mask = vec3(kMaskDark, kMaskDark, kMaskDark);
+	pos.x = fract(pos.x / 6.0);
+	if (pos.x < 0.333)
+		mask.r = kMaskLight;
+	else if (pos.x < 0.666)
+		mask.g = kMaskLight;
+	else
+		mask.b = kMaskLight;
+	return mask;
+}
+
+// Draw dividing bars.
+float bar(float pos, float bar) {
+	pos -= bar;
+	return (pos * pos < 4.0) ? 0.0 : 1.0;
+}
+
+float rand(vec2 co) {
+	return fract(sin(dot(co.xy , vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+// Entry.
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec2 pos = warp(fragCoord.xy / iResolution.xy);
+    vec4 unmodifiedColor = fetch(pos, vec2(0));
     
-#ifdef SCANLINES
-    float s = 1. - smoothstep(320., 1440., iResolution.y) + 1.;
-    float j = cos(y*iResolution.y*s)*.1; // values between .01 to .25 are ok.
-    color = abs(showScanlines-1.)*color + showScanlines*(color - color*j);
-    color *= 1. - ( .01 + ceil(mod( (st.x+.5)*iResolution.x, 3.) ) * (.995-1.01) )*showScanlines;
-#endif
+	// Unmodified.
+	if (fragCoord.x < iResolution.x * 0.333)
+	{
+		fragColor.rgb = unmodifiedColor.rgb;
+	}
+	else
+	{
+		if (fragCoord.x < iResolution.x * 0.666) {
+			sHardScan = -12.0;
+			kMaskDark = kMaskLight = 1.0;
+		}
+		fragColor.rgb = tri(pos) * mask(fragCoord.xy);
+	}
+	fragColor.rgb *=
+		bar(fragCoord.x, iResolution.x * 0.333) *
+		bar(fragCoord.x, iResolution.x * 0.666);
+	fragColor = vec4(
+        toSrgb(fragColor.rgb),
+        1.0
+    );
+}
 
-    // Border mask
-#ifdef CURVATURE
-        float m = max(0.0, 1. - 2.*max(abs(uv.x), abs(uv.y) ) );
-        m = min(m*200., 1.);
-        color *= m;
-#endif
-
-    // Color correction
-#ifdef COLOR_CORRECTION
-    color = postEffects(color, st);
-#endif
-
-	fragColor = vec4(max(vec3(.0), min(vec3(1.), color)), 1.);
+// H: The good thing about importing from Shadertoy is that FragColor and FragCoord are already implicit values
+void main() {
+    mainImage(gl_FragColor, gl_FragCoord.xy);
 }
